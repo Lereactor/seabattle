f0e8c6046ae9fdcbc4422d69f61d631099c6f5ca
Lereactor <lev@alaev.fr>
2020-03-06 01:31:03 +0300

    add miss target and class

--- js/seabattle.js
+++ js/seabattle.js
@@ -14,10 +14,37 @@ const play = {
     record: 0,
     shot: 0,
     hit: 0,
-    dead: 0
+    dead: 0,
+    set updateData(data) {
+        this[data] += 1;
+        this.render();
+    },
+    render() {
+        record.textContent = this.record;
+        shot.textContent = this.shot;
+        hit.textContent = this.hit;
+        dead.textContent = this.dead;
+    }
 }
-
-const fire = () => console.log('Fire!')
+//BD for target
+const show = {
+    hit(elem) {
+    },
+    miss(elem){
+        this.changeClass(elem, 'miss');
+    },
+    dead(elem){
+    },
+    changeClass(elem, value) {
+        elem.className = value;
+    }
+}
+//click to cell
+const fire = (event) => {
+    const target = event.target
+    show.miss(target);
+    play.updateData = 'shot';
+}
 //function for start
 const init = () => {
     enemy.addEventListener('click', fire)
