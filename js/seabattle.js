58f6b470b7ffece5f86e0f912e3c8e798e61151b
Lereactor <lev@alaev.fr>
2020-03-11 08:58:59 +0300

    add ships and game logic

--- js/seabattle.js
+++ js/seabattle.js
@@ -5,10 +5,34 @@ const record = document.getElementById('record');
 const shot = document.getElementById('shot');
 const hit = document.getElementById('hit');
 const dead = document.getElementById('dead');
+//Header gage title
+const header = document.querySelector('.header');
 //table space
 const enemy = document.getElementById('enemy');
 //button
 const again = document.getElementById('again');
+//ships
+const game = {
+    ships: [
+        {
+            location: ['26', '36', '46', '56'],
+            hit: ['', '', '', '']
+        },
+        {
+            location: ['11', '12', '13'],
+            hit: ['', '', '']
+        },
+        {
+            location: ['69', '79'],
+            hit: ['', '']
+        },
+        {
+            location: ['32'],
+            hit: ['']
+        }
+    ],
+    shipcount: 4,
+};
 //table for rate
 const play = {
     record: 0,
@@ -43,9 +67,32 @@ const show = {
 }
 //click to cell
 const fire = (event) => {
-    const target = event.target
+    const target = event.target;
+    if ((target.classList.length !== 0) || (target.tagName !== 'TD')) return;
     show.miss(target);
     play.updateData = 'shot';
+
+    for (let i = 0; i < game.ships.length; i++) {
+        const ship = game.ships[i];
+        const index = ship.location.indexOf(target.id);
+        if (index >= 0) {
+            show.hit(target);
+            play.updateData = 'hit';
+            ship.hit[index] = 'x';
+            const life = ship.hit.indexOf('');
+            if (life < 0) {
+                play.updateData = 'dead';
+                for (const cell of ship.location) {
+                    show.dead(document.getElementById(cell));
+                }
+                game.shipcount -= 1;
+                if (game.shipcount < 1) {
+                    header.textContent = 'Game Over';
+                    header.style.color = 'Red';
+                }
+            }
+        }
+    }
 }
 //function for start
 const init = () => {
